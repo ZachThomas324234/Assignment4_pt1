24f3927fc0e1664b92088445b8785163bbc907c7
ZachThomas324234 <146828835+ZachThomas324234@users.noreply.github.com>
2024-01-25 16:25:53 +0000

    EatMeatanimation

--- Assets/Scripts/Heal.cs
+++ Assets/Scripts/Heal.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Heal : MonoBehaviour
 {
-    public AudioSource audioData;
+    public AudioSource meat1audio;
+    public AudioSource meat2audio;
+    public AudioSource meat3audio;
 
     public int PlayerMaxHealth = 100;
     public int PlayerHealth = 100;
@@ -15,40 +18,84 @@ public class Heal : MonoBehaviour
     public int PlayerDamage = 25;
 
     public int EnemyMaxHealth = 25;
-    public int EnemyDamage = 15;
+
+    public GameObject meat1;
+    public GameObject meat2;
+    public GameObject meat3;
+    public GameObject chicken;
+    public GameObject player;
+    public GameObject sphere;
 
     private Timers timers;
 
+    private bool isEating;
 
-    // Start is called before the first frame update
+    public Mouse mouse;
+
+    
+    //Start
     void Start()
     {
-
+        chicken.SetActive(false);
     }
-    
-    // Update is called once per frame
+
+
+    //Eat meat and heal "animation"
+    private IEnumerator MeatDestroy()
+    {
+        isEating = true;
+        MeatAmount -= 1;
+        chicken.SetActive(true);
+        meat1.SetActive(true);
+        meat2.SetActive(true);
+        meat3.SetActive(true);
+        yield return new WaitForSeconds(0.5f);
+        meat1audio.Play();
+        meat1.SetActive(false);
+        yield return new WaitForSeconds(0.5f);
+        meat2audio.Play();
+        meat2.SetActive(false);
+        yield return new WaitForSeconds(0.5f);
+        meat3audio.Play();
+        meat3.SetActive(false);
+        PlayerHealth += HealAmount;
+        yield return new WaitForSeconds(0.5f);
+        chicken.SetActive(false);
+        isEating = false;
+    }
+
+    //Update is called once per frame
     void Update()
     {
+        //Right click function
         if (Input.GetKeyDown(KeyCode.Mouse1))
-            {
-                PlayerHealth += HealAmount;
-                MeatAmount -= 1;
-                if (MeatAmount > -1)
-            {
-                audioData.Play();
+        {
+        if (MeatAmount > 0)
+        {
+            if (!isEating)
+            {  
+            StartCoroutine (MeatDestroy());
             }
-            }
+        }  
+        }
     
-    
-        
-    if (MeatAmount < 1)
+        //No meat = no heal
+        if (MeatAmount < 1)
         {
         HealAmount = 0;
         }
 
+        //Health overdose
         if (PlayerHealth >= PlayerMaxHealth)
         {
         PlayerHealth = PlayerMaxHealth;
+        }
+
+        //No health = die
+        if (PlayerHealth < 1)
+        {
+        SceneManager.LoadScene(1);
+        mouse.ShowMouse();
         }
     }
 }
---
+++ Assets/Scripts/SphereCollision.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereCollision : MonoBehaviour
+{
+    public int EnemyDamage = 15;
+    public Heal health;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("Player"))
+        {
+        health.PlayerHealth -= 20;
+        }
+    }
+}
