0269c23f83ccc2f2f7f98d559305fe3a2ead4e3e
ZachThomas324234 <146828835+ZachThomas324234@users.noreply.github.com>
2024-02-29 16:27:21 +0000

    the

--- Assets/Scripts/WaveSpawner.cs
+++ Assets/Scripts/WaveSpawner.cs
@@ -6,24 +6,64 @@ public class WaveSpawner : MonoBehaviour
 {
     [SerializeField] private float countdown;
 
-    private void Update()
-    {
-        countdown -= Time.deltaTime;
+    [SerializeField] private GameObject spawnPoint;
 
-        if (countdown <= 0)
+    public Wave[] waves;
+
+    public int currentWaveIndex = 0;
+
+    private bool readyToCountDown;
+
+    private void Start()
+    {
+        readyToCountDown = true;
+
+        for (int i = 0; i < waves.Length; i++)
         {
-            SpawnWave();
+            waves[i].enemiesLeft = waves[i].enemies.Length;
         }
     }
 
-    private void SpawnWave()
+    private void Update()
     {
+        if (readyToCountDown == true)
+        {
+            countdown -= Time.deltaTime;
+        }
 
+        if (countdown <= 0)
+        {
+            readyToCountDown = false;
+            countdown = waves[currentWaveIndex].timeToNextWave;
+            StartCoroutine(SpawnWave());
+        }
+
+        if(waves[currentWaveIndex].enemiesLeft == 0)
+        {
+            readyToCountDown = true;
+            currentWaveIndex++;
+        }
+    }
+
+    private IEnumerator SpawnWave()
+    {
+        for (int i = 0; i < waves[currentWaveIndex] .enemies.Length; i++)
+        {
+            Enemy enemy = Instantiate(waves[currentWaveIndex].enemies[i], spawnPoint.transform);
+
+            enemy.transform.SetParent(spawnPoint.transform);
+
+            yield return new WaitForSeconds(waves[currentWaveIndex].timeToNextEnemy);
+        }
     }
 }
 
 [System.Serializable]
 public class Wave
 {
+    public Enemy[] enemies;
+    public float timeToNextEnemy;
+    public float timeToNextWave;
 
+    [HideInInspector] public int enemiesLeft;
 }
